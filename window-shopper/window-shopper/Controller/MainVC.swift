5c90140abbef62a4d3a0754985f62a00c2aab3cb
Jenna Hakki <jenna@Jennas-MacBook-Pro.local>
2017-12-13 21:44:59 -0500

    Custom txt & input accessory

---
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Jenna Hakki on 12/13/17.
+//  Copyright © 2017 Jenna Hakki. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5944321156, blue: 0, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    @objc func calculate() {
+        print("We got here")
+    }
+
+}
+
---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,39 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Jenna Hakki on 12/13/17.
+//  Copyright © 2017 Jenna Hakki. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTxtField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+        
+        
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+
+}
